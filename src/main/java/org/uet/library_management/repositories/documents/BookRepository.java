6c0cdcae44dfbedf512f1de2163d9ef9c8e94406
mthang1201 <mthang1201@gmail.com>
2024-10-04 09:11:30 +0700

    LoanService completed

--- src/main/java/org/uet/library_management/repositories/documents/BookRepository.java
+++ src/main/java/org/uet/library_management/repositories/documents/BookRepository.java
@@ -28,6 +28,21 @@ public class BookRepository extends DocumentRepository {
                 document.getAvailableCopies());
     }
 
+//    public void addUsingIsbn(String isbn) {
+//        Document document = BooksApiService.find(isbn);
+//        Book book = (Book) document;
+//        String query = "INSERT INTO " + db_table + " (title, authors, publisher, " +
+//                "publishedDate, description, isbn, pageCount, categories, averageRating, " +
+//                "ratingsCount, imageLinks, language, maturityRating, printType, " +
+//                "availableCopies) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+//        connectJDBC.executeUpdate(query, document.getTitle(), document.getAuthors(),
+//                book.getPublisher(), document.getPublishedDate(), document.getDescription(),
+//                book.getIsbn(), book.getPageCount(), document.getCategories(),
+//                book.getAverageRating(), book.getRatingsCount(), book.getImageLinks(),
+//                document.getLanguage(), book.getMaturityRating(), book.getPrintType(),
+//                document.getAvailableCopies());
+//    }
+
     @Override
     public void update(Document document) {
         Book book = (Book) document;
