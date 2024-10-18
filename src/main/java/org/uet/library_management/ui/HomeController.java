91670f3a5344aa2cc6b1c49759e8f24ba4aab6ac
vuduc <vuduc.19052005@gmail.com>
2024-10-18 09:41:24 +0700

    add scroll

--- src/main/java/org/uet/library_management/ui/HomeController.java
+++ src/main/java/org/uet/library_management/ui/HomeController.java
@@ -3,6 +3,7 @@ package org.uet.library_management.ui;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.FlowPane;
@@ -20,16 +21,24 @@ import java.util.List;
 
 public class HomeController {
     @FXML
+    public ScrollPane scrollpane;
+
+    @FXML
     public FlowPane flowPane;
 
     @FXML
     public void initialize() {
+        scrollpane.setContent(flowPane);
+        scrollpane.setFitToWidth(true);
+        scrollpane.setPannable(true);
+
         flowPane.setPadding(new Insets(10,10,10,10));
+
         Book book1 = new Book();
 
         SearchContext test = new SearchContext();
         test.setStrategy(new SearchByAuthor());
-        List<Book> searchTest2 = test.executeSearch("Eto Mori");
+        List<Book> searchTest2 = test.executeSearch("python");
         searchTest2.sort(new SortByAvgRating());
 
         for (Book book : searchTest2) {
@@ -42,6 +51,7 @@ public class HomeController {
             Label authorsLabel = new Label(book.getAuthors());
 
             vbox.getChildren().addAll(imageView, titleLabel, authorsLabel);
+
             flowPane.getChildren().add(vbox);
         }
     }
