0e9db7ef2f688fbf8a0564550af83fe5dcac9e4c
mthang1201 <mthang1201@gmail.com>
2024-10-29 16:21:48 +0700

    fix ui

--- src/main/java/org/uet/library_management/tools/Mediator.java
+++ src/main/java/org/uet/library_management/tools/Mediator.java
@@ -6,31 +6,4 @@ public class Mediator {
     public static String text;
 
     public static Book book;
-
-    private static Mediator instance;
-
-    private Mediator() {}
-
-    public static Mediator getInstance() {
-        if (instance == null) {
-            instance = new Mediator();
-        }
-        return instance;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        Mediator.text = text;
-    }
-
-    public Book getCachedBook() {
-        return book;
-    }
-
-    public void setCachedBook(Book book) {
-        Mediator.book = book;
-    }
 }
