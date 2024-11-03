7e4c87157e4e3d168da5059312f85720d6af18b3
mthang1201 <mthang1201@gmail.com>
2024-11-03 14:32:11 +0700

    update bookshelf

--- src/main/java/org/uet/library_management/ui/BookshelfController.java
+++ src/main/java/org/uet/library_management/ui/BookshelfController.java
@@ -1,4 +1,45 @@
 package org.uet.library_management.ui;
 
+import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.FlowPane;
+import org.uet.library_management.core.entities.documents.Book;
+import org.uet.library_management.core.services.documents.BookService;
+import org.uet.library_management.tools.UIBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BookshelfController {
+    @FXML
+    public ScrollPane verticalScrollpane;
+
+    @FXML
+    public FlowPane flowPane;
+
+//    private static List<Book> booksCache = new ArrayList<>();
+
+    @FXML
+    public void initialize() {
+        flowPane.setPadding(new Insets(10,10,10,10));
+
+//        SearchContext test = new SearchContext();
+//        test.setStrategy(new SearchByAuthor());
+//        List<Book> searchTest2 = test.executeSearch("python");
+//        searchTest2.sort(new SortByAvgRating());
+
+        BookService service = new BookService();
+
+//        if (booksCache.isEmpty()) {
+//            booksCache = service.findAll();
+//        }
+//        List<Book> books = booksCache;
+        List<Book> books = service.findAll();
+
+        flowPane.getChildren().addAll(
+                UIBuilder.createFlowPane(books).getChildren()
+        );
+
+    }
 }
