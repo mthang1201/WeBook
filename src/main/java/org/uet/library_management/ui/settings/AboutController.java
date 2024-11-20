9262d6fddd5d4d33c66a2e693726d73648c65606
hoangduong19 <hoangduong190105@gmail.com>
2024-11-20 07:53:01 +0700

    implement layout copyright

--- src/main/java/org/uet/library_management/ui/settings/AboutController.java
+++ src/main/java/org/uet/library_management/ui/settings/AboutController.java
@@ -1,8 +1,10 @@
 package org.uet.library_management.ui.settings;
 
 import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
 import lombok.SneakyThrows;
 import org.uet.library_management.SceneManager;
+import org.uet.library_management.tools.ImageLoaderUtil;
 
 /**
  * Controller class for handling events and actions in the "About" section of the application.
@@ -10,6 +12,18 @@ import org.uet.library_management.SceneManager;
  * This class is responsible for managing the interaction logic and UI transitions associated with the "About" section.
  */
 public class AboutController {
+    @FXML
+    private ImageView logoImage;
+
+    @SneakyThrows
+    @FXML
+    public void initialize() {
+        logoImage.setImage(ImageLoaderUtil.getImage("title.png"));
+        logoImage.setFitWidth(200);
+        logoImage.setFitHeight(50);
+        logoImage.setPreserveRatio(true);
+    }
+
     /**
      * Handles the back button action in the "About" section, triggering a transition to the settings scene.
      *
